b09baa61483d87393c1f6458f57ba67346a39d42
Soumitra <email@email.com>
2021-03-10 15:38:09 +0530

    moved to https://github.com/roytuts/codeigniter/tree/master/codeIgniter-multi-language-site

--- codeIgniter-3.1.10-multi-language-site/application/language/english/locale_lang.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$lang['msg']= 'Internationalization Example in Codeigniter';
-$lang['welcome']= 'Welcome to English Language';
-$lang['chooseLang']= 'Choose your language';
-$lang['copyright']= 'Copyright';
-$lang['year']= '2019';
--- codeIgniter-3.1.10-multi-language-site/application/language/french/locale_lang.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$lang['msg']= 'Internationalisation exemple dans Codeigniter';
-$lang['welcome']= 'Bienvenue à la langue française';
-$lang['chooseLang']= 'Choisissez votre langue';
-$lang['copyright']= 'Droit d\'auteur';
-$lang['year']= '2019';
