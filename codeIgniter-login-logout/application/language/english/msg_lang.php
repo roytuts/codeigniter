aa062f1d7b3bba2d97cbbd4dbc5b4dd0dca38a12
roytuts <projwebinfo@gmail.com>
2021-10-10 15:00:37 +0530

    codeigniter 3 login logout

---
+++ codeIgniter-login-logout/application/language/english/msg_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['incorrect_credentials'] = 'Incorrect username or password';
+$lang['login_success'] = 'You have been successfully logged in';
+$lang['logout_success'] = 'You have been successfully logged out';
+
+/* End of file msg_lang.php */
+/* Location: ./application/language/english/msg_lang.php */
