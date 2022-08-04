98e7716b631ece63c2b77bb57bedc913c4ebb20d
Soumitra <projwebinfo@gmail.com>
2022-08-04 14:49:49 +0530

    Create cdmodel.php

---
+++ codeigniter-datatable-ajax/application/models/cdmodel.php
@@ -0,0 +1,166 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/**
+ * Description of cdmodel
+ *
+ * @author https://roytuts.com
+ */
+class CdModel extends CI_Model {
+
+    private $cds = 'cds';
+
+    function __construct() {
+        
+    }
+
+    function get_cd_list() {
+        /* Array of table columns which should be read and sent back to DataTables. Use a space where
+         * you want to insert a non-database field (for example a counter or static image)
+         */
+        $aColumns = array(
+            'cd_id',
+            'cd_title',
+            'cd_interpret',
+            'cd_release_date',
+            'cd_no_of_copies',
+            'cd_type',
+            'cd_owner',
+            'cd_content_type');
+
+        /* Indexed column (used for fast and accurate table cardinality) */
+        $sIndexColumn = "cd_id";
+
+        /* Total data set length */
+        $sQuery = "SELECT COUNT('" . $sIndexColumn . "') AS row_count
+            FROM $this->cds";
+        $rResultTotal = $this->db->query($sQuery);
+        $aResultTotal = $rResultTotal->row();
+        $iTotal = $aResultTotal->row_count;
+
+        /*
+         * Paging
+         */
+        $sLimit = "";
+        $iDisplayStart = $this->input->get_post('start', true);
+        $iDisplayLength = $this->input->get_post('length', true);
+        if (isset($iDisplayStart) && $iDisplayLength != '-1') {
+            $sLimit = "LIMIT " . intval($iDisplayStart) . ", " .
+                    intval($iDisplayLength);
+        }
+
+        $uri_string = $_SERVER['QUERY_STRING'];
+        $uri_string = preg_replace("/%5B/", '[', $uri_string);
+        $uri_string = preg_replace("/%5D/", ']', $uri_string);
+
+        $get_param_array = explode("&", $uri_string);
+        $arr = array();
+        foreach ($get_param_array as $value) {
+            $v = $value;
+            $explode = explode("=", $v);
+            $arr[$explode[0]] = $explode[1];
+        }
+
+        $index_of_columns = strpos($uri_string, "columns", 1);
+        $index_of_start = strpos($uri_string, "start");
+        $uri_columns = substr($uri_string, 7, ($index_of_start - $index_of_columns - 1));
+        $columns_array = explode("&", $uri_columns);
+        $arr_columns = array();
+        foreach ($columns_array as $value) {
+            $v = $value;
+            $explode = explode("=", $v);
+            if (count($explode) == 2) {
+                $arr_columns[$explode[0]] = $explode[1];
+            } else {
+                $arr_columns[$explode[0]] = '';
+            }
+        }
+
+        /*
+         * Ordering
+         */
+        $sOrder = "ORDER BY ";
+        $sOrderIndex = $arr['order[0][column]'];
+        $sOrderDir = $arr['order[0][dir]'];
+        $bSortable_ = $arr_columns['columns[' . $sOrderIndex . '][orderable]'];
+        if ($bSortable_ == "true") {
+            $sOrder .= $aColumns[$sOrderIndex] .
+                    ($sOrderDir === 'asc' ? ' asc' : ' desc');
+        }
+
+        /*
+         * Filtering
+         */
+        $sWhere = "";
+        $sSearchVal = $arr['search[value]'];
+        if (isset($sSearchVal) && $sSearchVal != '') {
+            $sWhere = "WHERE (";
+            for ($i = 0; $i < count($aColumns); $i++) {
+                $sWhere .= $aColumns[$i] . " LIKE '%" . $this->db->escape_like_str($sSearchVal) . "%' OR ";
+            }
+            $sWhere = substr_replace($sWhere, "", -3);
+            $sWhere .= ')';
+        }
+
+        /* Individual column filtering */
+        $sSearchReg = $arr['search[regex]'];
+        for ($i = 0; $i < count($aColumns); $i++) {
+            $bSearchable_ = $arr['columns[' . $i . '][searchable]'];
+            if (isset($bSearchable_) && $bSearchable_ == "true" && $sSearchReg != 'false') {
+                $search_val = $arr['columns[' . $i . '][search][value]'];
+                if ($sWhere == "") {
+                    $sWhere = "WHERE ";
+                } else {
+                    $sWhere .= " AND ";
+                }
+                $sWhere .= $aColumns[$i] . " LIKE '%" . $this->db->escape_like_str($search_val) . "%' ";
+            }
+        }
+
+
+        /*
+         * SQL queries
+         * Get data to display
+         */
+        $sQuery = "SELECT SQL_CALC_FOUND_ROWS " . str_replace(" , ", " ", implode(", ", $aColumns)) . "
+        FROM $this->cds
+        $sWhere
+        $sOrder
+        $sLimit
+        ";
+        $rResult = $this->db->query($sQuery);
+
+        /* Data set length after filtering */
+        $sQuery = "SELECT FOUND_ROWS() AS length_count";
+        $rResultFilterTotal = $this->db->query($sQuery);
+        $aResultFilterTotal = $rResultFilterTotal->row();
+        $iFilteredTotal = $aResultFilterTotal->length_count;
+
+        /*
+         * Output
+         */
+        $sEcho = $this->input->get_post('draw', true);
+        $output = array(
+            "draw" => intval($sEcho),
+            "recordsTotal" => $iTotal,
+            "recordsFiltered" => $iFilteredTotal,
+            "data" => array()
+        );
+
+        foreach ($rResult->result_array() as $aRow) {
+            $row = array();
+            foreach ($aColumns as $col) {
+                $row[] = $aRow[$col];
+            }
+            $output['data'][] = $row;
+        }
+
+        return $output;
+    }
+
+}
+
+/* End of file cdmodel.php */
+/* Location: ./application/models/cdmodel.php */
